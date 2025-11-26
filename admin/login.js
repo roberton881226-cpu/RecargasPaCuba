932de9c4598985f3cd34dc33c9c2a070b3cfab14
roberton881226-cpu <roberton881226@gmail.com>
2025-11-26 12:54:38 +0000

    Update login.js

--- admin/login.js
+++ admin/login.js
@@ -1,1 +1,20 @@
+document.getElementById("admin-login").addEventListener("submit", function(e) {
+    e.preventDefault();
 
+    const user = document.getElementById("usuario").value.trim();
+    const pass = document.getElementById("clave").value.trim();
+
+    // Usuario y contraseña TEMPORALES
+    const adminUser = "admin";
+    const adminPass = "1234";
+
+    if (user === adminUser && pass === adminPass) {
+        // Guardamos sesión simple
+        localStorage.setItem("adminLogged", "true");
+
+        // Redirigimos al dashboard
+        window.location.href = "dashboard.html";
+    } else {
+        alert("Usuario o contraseña incorrectos");
+    }
+});
